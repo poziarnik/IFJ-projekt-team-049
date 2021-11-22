029a46484407b74665b8b736ef79c664c908160e
poziarnik <jurko.hatala@gmail.com>
2021-11-22 20:21:09 +0100

    whatever

--- symtable.c
+++ symtable.c
@@ -1,7 +1,7 @@
 /**
  *
  * @file symtable.c
- * @author Daniel Zalezak
+ * @author Juraj Hatala
  * @brief 
  * @date 
  *
@@ -9,8 +9,99 @@
 
 #include "symtable.h"
 #include <stdio.h>
+#include <stdbool.h>
 
-void tableInit(tSymbolTable *T){
-    T->root = NULL;
+void bst_insert(TreeItem **tree, char* key, tData* data) {
+  
+  if ((*tree) == NULL){
+    TreeItem *new = (TreeItem*)malloc(sizeof(TreeItem));
+    new->key = key;
+    new->data = data;
+    new->lptr = NULL;
+    new->rptr = NULL;
+    (*tree) = new;
+  }
+  else{
+    if (strcmp(key, (*tree)->key)<0){
+      bst_insert(&((*tree)->lptr), key, data);
+    }
+    else{
+      if (strcmp(key, (*tree)->key)>0){
+        bst_insert(&((*tree)->rptr), key, data);
+      }
+      else{
+        (*tree)->data = data;
+      }  
+    }  
+  }
 }
 
+bool bst_search(TreeItem *tree, char* key) {
+  if (tree == NULL){
+    return false;
+  }
+  else{
+    if (strcmp(key, tree->key)==0){
+      return true;
+    }
+    else{
+      if (strcmp(key, tree->key)<0){
+        bst_search(tree->lptr, key);
+      }
+      else{
+        bst_search(tree->rptr, key);
+      }
+    }
+  }
+  return false;
+}
+
+void stackPop(sym_stack *myStack){
+    sym_element* tmp;
+    if (myStack->head!=NULL){
+        tmp=myStack->head;
+        myStack->head=tmp->next;
+        free(tmp);
+    }
+}
+ 
+int stackPush(sym_stack* myStack, TreeItem* tree){
+    sym_element* newSymbol=(sym_element*)malloc(sizeof(sym_element));
+    if (newSymbol==NULL){
+        return 1;
+    }
+    newSymbol->tree=tree;
+    if (myStack->head!=NULL){
+        newSymbol->next=myStack->head;
+        myStack->head=newSymbol;
+    }
+    else{
+        myStack->head=newSymbol;
+    }
+    return 0;
+}
+void stackTop(sym_stack* myStack, TreeItem* tree){
+    if (myStack->head!=NULL){
+        tree=myStack->head->tree;
+    }
+}
+
+int treeCreate(TreeItem *newTree){
+    newTree=(TreeItem*)malloc(sizeof(TreeItem));
+    if(newTree==NULL){
+        return 3;
+    }
+    treeInit(newTree);
+    return 0;
+}
+void treeInit(TreeItem *newTree){
+    newTree=NULL;
+}
+int createSubTree(sym_stack* myStack){                        //pri volani funkcie cyklu atd...
+    TreeItem* newtree;
+    treeCreate(newtree);
+    stackPush(myStack, newtree);
+}
+//funkcia na vyhladavanie identifikatora
+//funkcia na ulozenie identifikatora<funkcia na vyhladavanie identifikatora
+int saveIdentifier()
