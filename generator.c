779b7668c0556a96fcbf8eebb6b53f23cdb3937b
xsao <dzalezak@gmail.com>
2021-11-25 00:38:08 +0100

    added instructions

--- generator.c
+++ generator.c
@@ -8,54 +8,90 @@
  */
 
 #include "generator.h"
-#include "symtable.h"
 #include "ilist.h"
 
 #include <stdio.h>
 #include <string.h>
 
+//TODO	20,204,230,234
+
 /******************** generovanie funkcii ********************/
+int interpret(tFunc *function){
+    //TODO 
+}
 
-
-void gen_func_begin(char *id){
-    if(strcmp(id, "main") == 0){
-        printf("\n LABEL $$main\n\
-                CREATEFRAME\n\
-                PUSHFRAME\n");
-    }
-    else{
+void gen_func_begin(tFunc *function){  
+        char *func_name = function->start;          
         printf("\n LABEL $%s\
-        \n PUSHFRAME\n", id);
-    }
+                \n CREATEFRAME\
+                \n PUSHFRAME\n", func_name);
 }
 
-void gen_func_end(char *id){
-    if (strcmp(id, "main") == 0)
-    {
-        printf(" CLEARS\n\
-                RETURN $END OF MAIN\n");
-    }
-    else{
-        printf(" LABEL $ end\n\
-                POPFRAME\n\
-                RETURN\n");
-    }
+void gen_func_end(tFunc *function){
+        char *func_name = function->start;
+        printf("\nLABEL $%s$end\
+                \nPOPFRAME\
+                \nRETURN\n", func_name);
 }
+ 
 
 void gen_func_call(char *id){
     printf(" CALL $%s \n", id);
 }
 
-void gen_func_def_arg(/*TODO*/){
-    printf(" DEFVAR TF@");
+void gen_func_def_arg(tFunc *function){
+    printf(" DEFVAR LF@%ls", function->param->par_num );
 }
 
-void gen_func_arg(/*TODO*/){
-    printf(" MOVE TF@");
+void gen_func_arg(tFunc *function){
+    printf(" MOVE LF@%s", function->param->id);
 }
 
-void gen_func_ret(int ret){
-    printf(" POPS LF@return%d \n", ret);
+void gen_func_ret(tFunc *function){
+    printf(" POPS LF@return%ls \n", function->retval);
+}
+
+/******************** generovanie operacii ********************/
+
+void gen_defvar(char *x){
+    printf("\n DEFVAR LF@%s", x);
+}
+
+void gen_move(char *x){                             //pri funkcii, MOVE LF@ LF@retval
+    printf("\n MOVE  LF@%s LF@%s", x ,x);                   
+}
+
+
+void gen_add(char *x){
+    printf("\n ADD      LF@retval LF@%s LF@%s" ,x, x);
+}
+
+void gen_sub(char *x){
+    printf("\n SUB      LF@retval LF@%s LF@%s", x, x);
+}
+
+void gen_mul(char *x){
+    printf("\n MUL      LF@retval LF@%s LF@%s" ,x, x);
+}
+
+void gen_div(char *x){
+    printf("\n DIV      LF@retval LF@%s LF@%s", x, x);
+}
+
+void gen_idiv(char *x){
+    printf("\n IDIV     LF@retval LF@%s LF@%s", x, x);
+}
+
+void gen_LT(char *x){
+    printf("\n LT       LF@retval LF@%s LF@%s", x, x);
+}
+
+void gen_GT(char *x){
+    printf("\n GT       LF@retval LF@%s LF@%s", x, x);
+}
+
+void gen_EQ(char *x){
+    printf("\n EQ       LF@result LF@%s LF@%s", x, x);
 }
 
 /******************** generovanie cyklov ********************/
@@ -171,9 +207,9 @@ void gen_write(){
 void gen_f2i(){
     printf("\n LABEL        $tointeger\
             \n PUSHFRAME\
-            \n DEFVAR       LF@param1\
-            \n DEFVAR       LF@retval0\
-            \n FLOAT2INT    LF@retval0 LF@param1\
+            \n DEFVAR       LF@param\
+            \n DEFVAR       LF@retval\
+            \n FLOAT2INT    LF@retval LF@param\
             \n POPFRAME\
             \n RETURN\n");
 }
@@ -181,9 +217,9 @@ void gen_f2i(){
 void gen_i2f(){
     printf("\n LABEL        $tofloat\
             \n PUSHFRAME\
-            \n DEFVAR       LF@param1\
-            \n DEFVAR       LF@retval0\
-            \n INT2FLOAT    LF@retval0 LF@param1\
+            \n DEFVAR       LF@param\
+            \n DEFVAR       LF@retval\
+            \n INT2FLOAT    LF@retval LF@param\
             \n POPFRAME\
             \n RETURN\n");
 }
@@ -218,7 +254,7 @@ void gen_chr(){
             \n RETURN\n");
 }
 
-int main(){                     //test
+/*int main(){                     //test
     
     gen_i2f();
     gen_read_i();
@@ -226,4 +262,28 @@ int main(){                     //test
     gen_f2i();
     gen_read_n();
     gen_read_s();
-}
+
+    char *x = "ahoj";
+
+    gen_defvar(x);
+
+    gen_move(x);
+
+    gen_add(x);
+
+    gen_sub(x);
+
+    gen_mul(x);
+
+    gen_div(x);
+
+    gen_idiv(x);
+
+    gen_LT(x);
+
+    gen_GT(x);
+        
+    gen_EQ(x);
+}*/
+
+
