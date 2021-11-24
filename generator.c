17d7d1baf1f423d8a1cf67b1275ff849feb3e04f
xsao <dzalezak@gmail.com>
2021-11-24 19:35:08 +0100

    added more inbuild func again

--- generator.c
+++ generator.c
@@ -19,7 +19,7 @@
 
 void gen_func_begin(char *id){
     if(strcmp(id, "main") == 0){
-        printf("\n LABEL $main\n\
+        printf("\n LABEL $$main\n\
                 CREATEFRAME\n\
                 PUSHFRAME\n");
     }
@@ -33,7 +33,7 @@ void gen_func_end(char *id){
     if (strcmp(id, "main") == 0)
     {
         printf(" CLEARS\n\
-                JUMP $EOF\n");
+                RETURN $END OF MAIN\n");
     }
     else{
         printf(" LABEL $ end\n\
@@ -80,27 +80,27 @@ void gen_then(){
 
 void gen_builtin_func(char *nieco){
     switch(*nieco){
-        case I_READI:
+        case readi:
             gen_read_i();
             break;
 
-        case I_READS:
+        case reads:
             gen_read_s();
             break;
 
-        case I_READN:
+        case readn:
             gen_read_n();
             break;
 
-        case I_WRITE:
+        case write:
             gen_write();
             break;
 
-        case I_F2I:
+        case tointeger:
             gen_f2i();
             break;
 
-        case I_I2F:
+        case tofloat:
             gen_i2f();
             break;
 
@@ -110,53 +110,53 @@ void gen_builtin_func(char *nieco){
 }
 
 int gen_read_i(){
-    printf("\n LABEL $inputi\
+    printf("\n LABEL        $readi\
             \n PUSHFRAME\
-            \n DEFVAR LF@retval0\
-            \n DEFVAR LF@retval1\
-            \n MOVE LF@retval1 int@0\
-            \n DEFVAR LF@typeout\
-            \n READ LF@retval0 int\
-            \n TYPE LF@typeout LF@retval0\
-            \n JUMPIFEQ $inputi$istrue LF@typeout string@int\
-            \n MOVE LF@retval0 int@1\
-            \n MOVE LF@retval1 int@1\
+            \n DEFVAR       LF@retval0\
+            \n DEFVAR       LF@retval1\
+            \n MOVE         LF@retval1 int@0\
+            \n DEFVAR       LF@typeout\
+            \n READ         LF@retval0 int\
+            \n TYPE         LF@typeout LF@retval0\
+            \n JUMPIFEQ     $readi$istrue LF@typeout string@int\
+            \n MOVE         LF@retval0 int@1\
+            \n MOVE         LF@retval1 int@1\
             \n POPFRAME\
             \n RETURN\
-            \n LABEL $inputi$istrue\
+            \n LABEL        $readi$istrue\
             \n POPFRAME\
             \n RETURN\n");   
 }
 
 int gen_read_s(){
-    printf("\n LABEL $inputs\
+    printf("\n LABEL        $reads\
             \n PUSHFRAME\
-            \n DEFVAR LF@retval0\
-            \n DEFVAR LF@retval1\
-            \n MOVE LF@retval1 int@0\
-            \n READ LF@retval0 string\
-            \n JUMPIFNEQ $inputs$noerr LF@retval0 nil@nil\
-            \n MOVE LF@retval1 int@1\
-            \n LABEL $inputs$noerr\
+            \n DEFVAR       LF@retval0\
+            \n DEFVAR       LF@retval1\
+            \n MOVE         LF@retval1 int@0\
+            \n READ         LF@retval0 string\
+            \n JUMPIFNEQ    $reads$noerr LF@retval0 nil@nil\
+            \n MOVE         LF@retval1 int@1\
+            \n LABEL        $reads$noerr\
             \n POPFRAME\
             \n RETURN\n");
 }
 
 int gen_read_n(){
-    printf("\n LABEL $inputf\
+    printf("\n LABEL        $readn\
             \n PUSHFRAME\
-            \n DEFVAR LF@retval0\
-            \n DEFVAR LF@retval1\
-            \n MOVE LF@retval1 int@0\
-            \n DEFVAR LF@typeout\
-            \n READ LF@retval0 float\
-            \n TYPE LF@typeout LF@retval0\
-            \n JUMPIFEQ $inputf$istrue LF@typeout string@float\
-            \n MOVE LF@retval0 float@0x1p+0\
-            \n MOVE LF@retval1 int@1\
+            \n DEFVAR       LF@retval0\
+            \n DEFVAR       LF@retval1\
+            \n MOVE         LF@retval1 int@0\
+            \n DEFVAR       LF@typeout\
+            \n READ         LF@retval0 float\
+            \n TYPE         LF@typeout LF@retval0\
+            \n JUMPIFEQ     $readn$istrue LF@typeout string@float\
+            \n MOVE         LF@retval0 float@0x1p+0\
+            \n MOVE         LF@retval1 int@1\
             \n POPFRAME\
             \n RETURN\
-            \n LABEL $inputf$istrue\
+            \n LABEL        $readn$istrue\
             \n POPFRAME\
             \n RETURN\n");
 }
@@ -165,24 +165,25 @@ void gen_write(){
     printf("\n LABEL $write\
             \n PUSHFRAME\
             \n");
+            //TODO
 }
 
 void gen_f2i(){
-    printf("\n LABEL $float2int\
+    printf("\n LABEL        $tointeger\
             \n PUSHFRAME\
-            \n DEFVAR LF@param1\
-            \n DEFVAR LF@retval0\
-            \n FLOAT2INT LF@retval0 LF@param1\
+            \n DEFVAR       LF@param1\
+            \n DEFVAR       LF@retval0\
+            \n FLOAT2INT    LF@retval0 LF@param1\
             \n POPFRAME\
             \n RETURN\n");
 }
 
 void gen_i2f(){
-    printf("\n LABEL $int2float\
+    printf("\n LABEL        $tofloat\
             \n PUSHFRAME\
-            \n DEFVAR LF@param1\
-            \n DEFVAR LF@retval0\
-            \n INT2FLOAT LF@retval0 LF@param1\
+            \n DEFVAR       LF@param1\
+            \n DEFVAR       LF@retval0\
+            \n INT2FLOAT    LF@retval0 LF@param1\
             \n POPFRAME\
             \n RETURN\n");
 }
@@ -198,12 +199,31 @@ void gen_ord(){
 }
 
 void gen_chr(){
-//TODO
+    printf("\n LABEL        $chr\
+            \n PUSHFRAME\
+            \n DEFVAR       LF@retval1\
+            \n DEFVAR       LF@retval0\
+            \n GT           LF@retval1 LF@param0 int@255\
+            \n JUMPIFEQ     $chr$error LF@retval1 bool@true\
+            \n LT           LF@retval1 LF@param0 int@0\
+            \n JUMPIFEQ     $chr$error LF@retval1 bool@true\
+            \n INT2CHAR     LF@retval0 LF@param0\
+            \n MOVE         LF@retval1 int@0\
+            \n POPFRAME\
+            \n RETURN\
+            \n LABEL        $chr$error\
+            \n MOVE         LF@retval0 string@\
+            \n MOVE         LF@retval1 int@1\
+            \n POPFRAME\
+            \n RETURN\n");
 }
 
 int main(){                     //test
-    char *e = "ahoj";
-    printf("\nLABEL $%s"\
-        "\nPUSHFRAME\n", e);
+    
     gen_i2f();
+    gen_read_i();
+    gen_chr();
+    gen_f2i();
+    gen_read_n();
+    gen_read_s();
 }
